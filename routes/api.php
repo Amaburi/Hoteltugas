14281bda6d208ef6391706ba8ef3ecbe31cb19b0
Amaburi <drsservant@gmail.com>
2022-11-25 20:47:45 +0700

    j

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\hotel\getuserid\GetUserIdController;
 use App\Http\Controllers\hotel\kamar\kamarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\profile\user\AuthController;
 use App\Http\Controllers\hotel\listhotel\ListController;
+use App\Http\Controllers\hotel\payment\PaymentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,29 +33,18 @@ Route::middleware('guest')->group(function() {
 Route::middleware('auth:sanctum')->group(function() {
     Route::post('logout', [AuthController::class, 'logout']);
     
-    // Get All user
     Route::prefix('admin')->middleware('role:admin')->group(function() {
         Route::resource('users', AuthController::class, ['only' => ['index']]);
-    });
-    // Hotel List
-    Route::prefix('admin')->middleware('role:admin')->group(function() {
         Route::resource('listhotel', ListController::class, ['only' => ['index','store','update']]);
-    });
-    Route::prefix('user')->middleware('role:user')->group(function() {
-        Route::resource('listhotel', ListController::class, ['only' => ['index']]);
-    });
-    // Room list
-    Route::prefix('admin')->middleware('role:admin')->group(function() {
         Route::resource('listkamar', kamarController::class, ['only' => ['index','store','update']]);
+        Route::resource('payment', PaymentController::class, ['only' => ['showall']]);
+        Route::resource('userid', GetUserIdController::class, ['only' => ['index','store']]);
     });
+    
     Route::prefix('user')->middleware('role:user')->group(function() {
-        Route::resource('listkamar', kamarController::class, ['only' => ['index']]);
-    });
-    // Payment
-    Route::prefix('admin')->middleware('role:admin')->group(function() {
-        Route::resource('payment', kamarController::class, ['only' => ['index']]);
-    });
-    Route::prefix('user')->middleware('role:user')->group(function() {
-        Route::resource('payment', kamarController::class, ['only' => ['index','store']]);
+        Route::resource('listshotel', ListController::class, ['only' => ['index']]);
+        Route::resource('listskamar', kamarController::class, ['only' => ['index']]);
+        Route::resource('payments/{userid}', PaymentController::class, ['only' => ['index','store']]);
+        Route::resource('userids/{nama}', GetUserIdController::class, ['only' => ['showw']]);
     });
 });
